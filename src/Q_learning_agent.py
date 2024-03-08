a36b0be6869f44fbfb225c4677526c885bb280a0
maberet <maberet@PC-Marc.>
2024-03-08 12:38:57 -0500

    Commit of the Q learning method

---
+++ src/Q_learning_agent.py
@@ -0,0 +1,144 @@
+import random
+import json
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+
+##set the parameters 
+# alpha is the learning rate
+# gamma is the discount factor
+# epsilon is the exploration factor
+# num_episodes is the number of episodes to train the agent
+# env is the environment
+# Q is the Q table
+
+# alpha = 0.01
+# n_episodes = 100_000
+
+
+class Agent:
+    def __init__(self,env):
+        self.env = env
+        self.Q = {}
+        self.alpha = 0.01
+        self.gamma = 0.9
+        self.start_epsilon = 1.0
+        self.final_epsilon = 0.01   
+        self.epsilon = self.start_epsilon
+        self.TDdiff=[]
+
+    def initialize_Q(self):
+        for player_sum in range(1,33): # 1 to 32 cards
+            for dealer_card in range(1,12): # 1 to 11 cards, 11 means dealer has an ace 
+                for action in range(0,2):
+                    self.Q[(player_sum,dealer_card,action)] = [0.0,0.0]#[random.randint(1, 100)/100,random.randint(1, 100)/100] # is the initial value of Q(s,a) for all s and a
+    
+    def print_Q(self):  
+        for key, value in self.Q.items():
+            print(key, value)
+    
+    def getQ(self):
+        return self.Q
+
+    def update_Q(self, state, action, reward, next_state,done):
+        future_q = (not done)*max(self.Q.get(next_state)[0],self.Q.get(next_state)[1])
+        td_diff = reward + self.gamma*future_q - self.Q[state][action]
+        self.Q[state][action] = self.Q[state][action] + self.alpha*(td_diff)
+        self.TDdiff.append(td_diff)
+
+    def get_new_action(self, state):
+        if random.randint(1, 100)/100 < self.epsilon:
+            return self.env.action_space.sample()## random action retrieve a random action between 0 and 1
+        else:
+            maxi= max(self.Q.get(state)[0],self.Q.get(state)[1]) ## greedy action take the max of 
+            if maxi == self.Q.get(state)[0]:
+                return 0
+            else:
+                return 1
+
+    def train(self, num_episodes, epsilon):
+        self.initialize_Q()
+        self.epsilon = epsilon
+        numberwin = 0
+        numberdraw = 0
+        numberloose = 0
+        winratebyepisode = []
+        for episode in tqdm(range(num_episodes)):
+            done = False
+            state,info = self.env.reset()
+            self.epsilon = self.final_epsilon + (self.start_epsilon - self.final_epsilon) * (num_episodes - episode) / num_episodes
+            while not done:
+                action = self.get_new_action(state)
+                next_state, reward, done, truncated,info = self.env.step(action)
+                self.update_Q(state, action, reward, next_state, done)
+                done = done or truncated
+                state = next_state
+            if reward == 1:
+                numberwin += 1
+            elif reward == 0:
+                numberdraw += 1
+            else:
+                numberloose += 1
+            winratebyepisode.append([episode, numberwin/(episode+1)])
+                
+        self.plot_winrate(winratebyepisode)
+        self.plot_game_stats(numberwin, numberdraw, numberloose)
+        print("Training done")
+        print("Win rate: ", numberwin/num_episodes)
+        # policy = create_policy_table(self.Q)
+        # print(policy)
+        self.save_Q()# Save the Q table to a file after training
+
+    def plot_winrate(self,winratebyepisode):
+        plt.plot([x[0] for x in winratebyepisode], [x[1] for x in winratebyepisode])
+        plt.xlabel('Episode')
+        plt.ylabel('Win rate')
+        plt.title('Win rate by episode')
+        plt.show()
+
+    def pot_td_diff(self):
+        plt.plot(self.TDdiff)
+        plt.xlabel('Episode')
+        plt.ylabel('TD difference')
+        plt.title('TD difference by episode')
+        plt.show()
+
+    def plot_game_stats(self, numberwin, numberdraw, numberloose):
+        labels = 'Win', 'Draw', 'Loose'
+        sizes = [numberwin, numberdraw, numberloose]
+        colors = ['gold', 'yellowgreen', 'lightcoral']
+        explode = (0.1, 0, 0)   
+        plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+        plt.axis('equal')
+        plt.show()
+
+
+
+
+    # Save the Q table to a file 
+    # This is useful if you want to save the Q table after training and use it later
+    # self.Q is a dictionary, so we can use the json module to save it to a file
+    def save_Q(self):                           
+        with open('Q_table.json', 'w') as f:
+            #change all keys to string
+            self.Q = {str(k): v for k, v in self.Q.items()}
+
+            json.dump(self.Q, f)
+
+    # Load the Q table from a file
+    # This is useful if you want to load a Q table that you have saved previously
+    # self.Q is a dictionary, so we can use the json module to load it from a file
+    def import_Q(self):
+        with open('Q_table.json', 'r') as f:
+            self.Q = json.load(f)
+            #change all keys back to tuple
+            self.Q = {tuple(map(int, k.split(','))): v for k, v in self.Q.items()}
+            
+def create_policy_table(Qtable):
+    policy = {}
+    for key, value in Qtable.items():
+        if value[0] > value[1]:
+            policy[key] = 0
+        else:
+            policy[key] = 1
+    return policy
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
+import Q_learning_agent as ag
+import gymnasium as gym
+
 def main():
-    # Your code here
-    print("Hello, world!")
+    env = gym.make("Blackjack-v1", sab=True)
+
+    # reset the environment to get the first observation
+    agent = ag.Agent(env)
+    agent2= ag.Agent(env)
+    
+    agent.initialize_Q()
+    
+    agent.train(10000000, 0.1)
+
+    #agent.train(1000000, 0.1)
+    #agent.playWithModel()
+    
     return 0 # Success
 
 if __name__ == "__main__":
